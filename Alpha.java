05e3035a1f8296095b4f34264eab7cd753129089
loganathan1704 <47380567+loganathan1704@users.noreply.github.com>
2019-02-06 14:19:06 +0530

    Create Alpha.java

---
+++ Alpha.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Alpha
+    {
+        public static void main(String args[])
+        {
+            char c;
+            Scanner s=new Scanner(System.in);
+            System.out.println("Enter the alphabet");
+            c=s.next().charAt(0);
+            if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+            {
+                System.out.println("Alphabet");
+            }
+            else
+            {
+            System.out.println("No");   
+            }
+            
+        }
+    }
