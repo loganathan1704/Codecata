413853a13d58cd732828c8ab845f0389dde29c4d
loganathan1704 <47380567+loganathan1704@users.noreply.github.com>
2019-02-06 16:00:08 +0530

    Update Greatest.java

--- Greatest.java
+++ Greatest.java
@@ -4,10 +4,10 @@ class Greatest
 	public static void main(String args[]) 
 	{
 	    int n1,n2,n3;
-	    Scanner sc=new Scanner(System.in);
-		n1=sc.nextInt();
-		n2=sc.nextInt();
-		n3=sc.nextInt();
+	    Scanner g=new Scanner(System.in);
+		n1=g.nextInt();
+		n2=g.nextInt();
+		n3=g.nextInt();
 	    if((n1>n2)&&(n1>n3))
 	    {
 	       System.out.println(n1); 
