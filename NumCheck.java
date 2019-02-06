d9b48da445f1a100f59248a45d9267997435db85
loganathan1704 <47380567+loganathan1704@users.noreply.github.com>
2019-02-06 16:02:39 +0530

    Update NumCheck.java

--- NumCheck.java
+++ NumCheck.java
@@ -1,11 +1,10 @@
 import java.util.*;
-public class NumCheck
+class NumCheck
 {
 	public static void main(String[] args) 
 	{
 	    int num;
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the number:");
 		num=sc.nextInt();
 	    if(num>=1)
 	    {
