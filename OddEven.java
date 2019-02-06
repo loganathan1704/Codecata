0372e876037125ff4079fbbda9f2ff9df6e6fce5
loganathan1704 <47380567+loganathan1704@users.noreply.github.com>
2019-02-06 14:01:52 +0530

    Create OddEven.java

---
+++ OddEven.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class OddEven
+{
+	public static void main(String[] args) 
+	{
+	    int num;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		num=sc.nextInt();
+	    if(num%2==0)
+	    {
+	       System.out.println("Even"); 
+	    }
+	    else
+	    {
+	        System.out.println("Odd");
+	    }
+	    
+	    
+	}
+}
