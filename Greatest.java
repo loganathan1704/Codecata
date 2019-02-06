caedb81b9a236c3c777313026bc53e1f91c547f3
loganathan1704 <47380567+loganathan1704@users.noreply.github.com>
2019-02-06 15:27:09 +0530

    Update and rename Main.java to Greatest.java

---
+++ Greatest.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Greatest
+{
+	public static void main(String[] args) 
+	{
+	    int n1,n2,n3;
+	    Scanner sc=new Scanner(System.in);
+		System.out.println("Enter three numbers");
+		n1=sc.nextInt();
+		n2=sc.nextInt();
+		n3=sc.nextInt();
+	    if((n1>n2)&&(n1>n3))
+	    {
+	       System.out.println(n1); 
+	    }
+	    else if(((n2>n1)&&(n2>n3)))
+	    {
+	        System.out.println(n2);
+	    }
+	    else
+	    {
+	        System.out.println(n3);
+	    }
+	    
+	    
+	}
+}
