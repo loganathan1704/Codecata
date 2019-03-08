da342a419ba7ca25764dbbbf6a899487683e030a
loganathan1704 <47380567+loganathan1704@users.noreply.github.com>
2019-03-08 15:01:57 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Natural
+{
+	public static void main(String[] args) 
+	{
+	    int N,i,s=0;
+	    Scanner n=new Scanner(System.in);
+	    N=n.nextInt();
+	    for(i=1;i<=N;i++)
+	    {
+	        s=s+i;
+	        
+	    }
+	    System.out.print(s);
+	}
+}
