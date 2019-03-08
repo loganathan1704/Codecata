1210959373793febf1f50ef7519054cb7eb598f4
loganathan1704 <47380567+loganathan1704@users.noreply.github.com>
2019-03-08 14:26:20 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    int N,i;
+	    Scanner n=new Scanner(System.in);
+	    N=n.nextInt();
+	    for(i=1;i<=N;i++)
+	    {
+	        System.out.println("Hello");
+	    }
+	}
+}
