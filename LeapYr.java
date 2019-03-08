f1ecfc7f3838a3f0406f70560c5ae9ce0e71d5aa
loganathan1704 <47380567+loganathan1704@users.noreply.github.com>
2019-03-08 14:13:19 +0530

    Create LeapYr.java

---
+++ LeapYr.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class LeapYr
+{
+	public static void main(String[] args) 
+	{
+		int year;
+		Scanner y=new Scanner(System.in);
+		year=y.nextInt();
+		if(year%4==0)
+		{
+		    System.out.println("yes");
+		}
+		else
+		{
+		    System.out.println("no");
+		}
+		
+	}
+}
+	
