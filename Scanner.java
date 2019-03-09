90cf4a53d3cceb741e8b1dbda71b478f550d21a3
loganathan1704 <47380567+loganathan1704@users.noreply.github.com>
2019-03-09 10:40:42 +0530

    Create Scanner.java

---
+++ Scanner.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Scanner_ex
+{
+int n;
+String a;
+Scanner s=new Scanner(System.in);
+System.out.print("Enter integer");
+n=s.nextInt();
+System.out.print("Enter string");
+a=s,next();
+System.out.print("n");
+System.out.print("a");
