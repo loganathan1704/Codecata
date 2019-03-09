e42a60923bc78dca6cce9eed43055bb626d03b00
loganathan1704 <47380567+loganathan1704@users.noreply.github.com>
2019-03-09 10:47:10 +0530

    Update Scanner.java

--- Scanner.java
+++ Scanner.java
@@ -1,3 +1,4 @@
+//Scanner class
 import java.util.*;
 class Main
 {
@@ -6,8 +7,10 @@ class Main
     int n;
     String a;
     Scanner s=new Scanner(System.in);
+        //get the integer
     System.out.println("Enter integer");
     n=s.nextInt();
+        //get the string
     System.out.println("Enter string");
     a=s.next();
     System.out.print("n");
