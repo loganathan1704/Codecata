142617c16c82c91ee0d608363a36a5f8fcf7d1ff
loganathan1704 <47380567+loganathan1704@users.noreply.github.com>
2019-03-24 19:24:57 +0530

    Update ArraySum.java

--- ArraySum.java
+++ ArraySum.java
@@ -1,24 +1,21 @@
 import java.util.Scanner;
-public class Main 
+class ArraySum
 {
-	public static void main(String[] args) 
-	{
-	     int[] a=new int[100];
-	   int n,k,i,s=0;
-	Scanner e=new Scanner(System.in);
-	System.out.print("enter n");
-	 n=e.nextInt();
-	for(i=0;i<n;i++)
-	{
-	   
-	   a[i]=e.nextInt();
-    }
-    k=e.nextInt();
-	for(i=0;i<k;i++)
-	{
-		s=s+a[i];
-	}
-	
-  System.out.print(s);
+    public static void main(String[] args) 
+    {
+        int N,K,S=0;
+        Scanner l=new Scanner(System.in);
+        N=l.nextInt();
+        K=l.nextInt();
+        int a[] = new int[N];
+        for(int i=0;i<N;i++)
+        {
+            a[i]=l.nextInt();
+        }
+        for(int i=0;i<K;i++)
+        {
+            S+=a[i];
+        }
+        System.out.println(S);
     }
 }
